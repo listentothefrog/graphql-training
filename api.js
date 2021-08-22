826a8e91ade9437258a94994dc69ca48e97f9974
listentothefrog <shashanke7y@gmail.com>
2021-08-22 12:36:37 -0400

    basic graphql api

---
+++ api.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { ApolloServer, gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+    another: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+  Query: {
+    another: () => "Another query",
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const main = async () => {
+  const app = express();
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.listen({ port: 4000 }, () =>
+    console.log("Now browse to http://localhost:4000" + server.graphqlPath)
+  );
+};
+
+main();
